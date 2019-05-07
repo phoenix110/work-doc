4501ea0628850e0f808a88da6ada6a62851bef4d
xiaomuMK <xixiaomu163@163.com>
2019-05-07 18:18:35 +0800

    uuuu

--- 浙江省慢病监测系统/浙江省慢病监测sql.sql
+++ 浙江省慢病监测系统/浙江省慢病监测sql.sql
@@ -11,10 +11,17 @@ SELECT * FROM TB_LOG where rownum < 2;
 SELECT * FROM ZJJK_YW_LOG where rownum < 2;
 
 select * from sqldy where mkbh = '020205';-- 糖尿病初随访
-select * from sqldy where mkbh = '020302';-- 肿瘤初访
+select * from sqldy where mkbh = '020302' for update;-- 肿瘤初访
 select * from sqldy where mkbh = '020303'; -- 肿瘤随访
+select * from sqldy where mkbh = '020601'; -- 生命统计死亡管理
+select * from sqldy where mkbh = '020401'; -- 心脑血管
+
+
 
 -- 自己添加肿瘤随访提醒 VC_GLDW = '331002'为区县级别的
 -- update zjjk_zl_bgk set NB_KSPF = 40, DT_SFSJ = add_months(sysdate, -2) where VC_BGKID in ('ex1800000003696','ex1800000003697','ex1800000003698')；
 -- update zjjk_zl_bgk set VC_GLDW = '331002', vc_sdqrzt = '1', vc_sfcf = '3', dt_swrq = null, vc_swyy = null, vc_shbz = '3', vc_bgkzt = '0',NB_KSPF = 40, DT_SFSJ = add_months(sysdate, -2),dt_sfrq = add_months(sysdate, -2) where VC_BGKID in ('ex1800000003696','ex1800000003697','ex1800000003698');
  
+-- 自己添加糖尿病随访提醒 VC_GLDW = '331002'为区县级别的
+-- update zjjk_tnb_bgk set vc_gldw = '331002', vc_sdqrzt = '1', vc_shbz = '3', vc_scbz = '0', vc_tnblx = '1', vc_bgkzt = '0', vc_cfzt = '3', dt_sfsj = add_months(sysdate,-11) where vc_bgkid in ('2c90ee26699f5d6001699f64d2470003','6E69C432D3B050E6E05010AC296E5189');
+
