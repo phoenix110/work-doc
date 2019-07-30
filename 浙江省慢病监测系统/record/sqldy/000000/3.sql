f78afb461bd806667f02442994a10e6673248de2
xiaomuMK <xixiaomu163@163.com>
2019-07-30 16:27:27 +0800

    管理单位目前调用P_YLJG表，改为调用organ_node表

--- 浙江省慢病监测系统/record/sqldy/000000/3.sql
+++ 浙江省慢病监测系统/record/sqldy/000000/3.sql
@@ -1,7 +1,13 @@
+<if if(1==1)>
+select CODE, NAME from organ_node
+    where removed = 0
+    and description like '%' || #{xzqh} || '%'
+</if>
+<if if(1!=1)>
 select DM AS CODE, MC AS NAME
   from P_YLJG a
   where 1 = 1
     and a.xzqh like #{xzqh}||'%' 
     AND a.lb in ('B1','A1')
     and bz = 1
- 
+</if>    
